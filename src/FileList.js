728a0a55d8bbb6a60567156294a113e2b9b5d943
Toms Mikoss <toms.mikoss@gmail.com>
2017-04-06 22:57:37 +0300

    File list previews

--- src/FileList.js
+++ src/FileList.js
@@ -5,11 +5,20 @@ import IconButton from 'material-ui/IconButton'
 import Delete from 'material-ui/svg-icons/action/delete'
 import moment from 'moment'
 
+const styles = {
+  cell: {
+    display: 'flex'
+  },
+  preview: {
+    margin: '35px auto'
+  }
+}
+
 class Header extends PureComponent {
   render() {
     const { files, allUsers } = this.props
 
-    const tiles = map(files, ({ id, name, title, thumb_160, timestamp, user }) => {
+    const tiles = map(files, ({ id, name, title, thumb_160, timestamp, user, pretty_type }) => {
       const { name: userName } = allUsers[user] || {}
       const date = moment.unix(timestamp)
 
@@ -17,8 +26,12 @@ class Header extends PureComponent {
         <strong>{userName}</strong> @ {date.format('DD.MM.YYYY')}
       </span>
 
-      return <GridTile key={id} title={name} subtitle={subtitle} actionIcon={<IconButton><Delete color="white" /></IconButton>} >
-        <img src={thumb_160} alt={title}/>
+      const preview = thumb_160 ? <img src={thumb_160} alt={title} /> : <div style={styles.preview}>{pretty_type}</div>
+
+      const action = <IconButton><Delete color="white" /></IconButton>
+
+      return <GridTile key={id} title={name} subtitle={subtitle} actionIcon={action} style={styles.cell} >
+        {preview}
       </GridTile>
     })
 
